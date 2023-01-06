40cd907349bef5ad295d99c5391348fb41183d76
hohihohi <a56.tazima@gmail.com>
2023-01-06 13:24:11 +0900

    test: specify an array of file extensions my modules use

--- jest.config.ts
+++ jest.config.ts
@@ -76,16 +76,16 @@ export default {
   // ],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
+  moduleFileExtensions: [
+    "js",
   //   "mjs",
   //   "cjs",
   //   "jsx",
-  //   "ts",
+    "ts",
   //   "tsx",
-  //   "json",
+    "json",
   //   "node"
-  // ],
+  ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
