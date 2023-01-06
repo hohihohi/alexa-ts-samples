94d5aa0e28b0984d8f86fcc5ef730c8feb9d2a48
hohihohi <a56.tazima@gmail.com>
2023-01-06 13:41:38 +0900

    style: enable to apply lint to jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
 module.exports = {
 	'env': {
 		'browser': true,
-		'es2021': true
+		'es2021': true,
+    'jest/globals': true
 	},
 	'extends': [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended'
+		'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended'
 	],
 	'overrides': [
 	],
@@ -15,7 +17,8 @@ module.exports = {
 		'sourceType': 'module'
 	},
 	'plugins': [
-		'@typescript-eslint'
+		'@typescript-eslint',
+    'jest'
 	],
 	'rules': {
 		'indent': [
